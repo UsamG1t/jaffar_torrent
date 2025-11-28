c7a0e225d3095f7afdc443be693b78efb75b1d73
Kate <fukuro.s@yandex.ru>
2025-11-28 15:22:50 +0300

    added templates

---
+++ Taskology/testing_system/VA.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct VA {
+    // опишите структуру данных
+};
+
+unsigned int VA_to_PA(unsigned int VA) {
+    // функция, получающая на вход виртуальный адрес и преобразующая его в физический
+    return 1;
+}
---
+++ Taskology/testing_system/VirtIntoPhys.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Seg {
+} segment;
+
+unsigned int VirtIntoPhys (segment *SegTable, unsigned int VirtAdr) {
+    //виртуальный сегментный адрес VirtAdr, массив структур SegTable
+    return 1;
+}
